7a0e9be50c5a3cfa14c2857bd727129c71e108dd
Nazim Mahmud <nazim1.ma@gmail.com>
2025-11-10 10:41:50 +0600

    added python syntax project

---
+++ python_syntax/README.txt
@@ -0,0 +1,39 @@
+https://flask.palletsprojects.com/en/stable/
+
+Step 1: Install Python
+Check version: python --version
+
+Step 2: Create a Virtual Environment(use cmd command)
+
+python3 -m venv .venv
+source .venv/Scripts/activate
+
+
+Step 3: 
+Install Flask:
+pip install Flask
+
+Check installation:
+pip show flask
+
+Step 4: Create a Simple Flask App
+app.py
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Flask! 🚀"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+Run it: (use cmd command)
+python3 -m venv .venv
+source .venv/Scripts/activate
+pip install Flask
+python app.py
+
---
+++ python_syntax/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    users = ['Alice', 'Bob', 'Charlie']
+    return render_template('about.html', users=users)
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return render_template('user.html', username=username)
+
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    username = request.form.get('username')
+    # return f"Form submitted! Hello, {username}!"
+    return render_template('user_info.html', username=username)
+
+if __name__ == '__main__':
+    app.run(debug=True)
