29a73ce786a17ef0a1bfe16f5644788394f9d1f4
Nazim Mahmud <nazim1.ma@gmail.com>
2025-11-10 11:14:57 +0600

    updated

---
+++ python_syntax/syntax.py
@@ -0,0 +1,30 @@
+""" if 5 > 2:
+ print("Five is greater than two!") 
+
+if 5 > 2:
+        print("Five is greater than two!")  """
+
+""" y = "Hello, World!"
+print(y) """
+
+""" print(3 + 3)
+print(2 * 5) """
+
+# Mix Text and Numbers
+# print("I am", 35, "years old.")
+
+# multiple variable concat
+# String Concatenation
+""" a = 'Md.'
+b = 'Nazim'
+c = 'Mahmud'
+print(a, b, c) """
+
+""" x = "Python is "
+y = "awesome"
+z =  x + y
+print(z) """
+
+x = 5
+y = 10
+print(x + y)
